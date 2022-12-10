eb18a8513c5bf5f3a5cbbf0bd3ee63eac733b38f
chitrang <chitrang.patel@ncoregames.com>
2022-12-10 12:29:12 +0530

    form update and add functionality

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from "next/head";
+import { useState } from "react";
 import { BiUserPlus } from "react-icons/bi";
 import Form from "../components/form";
 import Table from "../components/table";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+
+  const onClickAddEmployee = () => {
+    setVisible(!visible);
+  };
   return (
     <section>
       <Head>
@@ -18,8 +24,11 @@ export default function Home() {
         </h1>
         <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex gap-3">
-            <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-500 hover:border-indigo-500 hover:text-gray-800-500">
-              Add Employee{" "}
+            <button
+              onClick={onClickAddEmployee}
+              className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-500 hover:border-indigo-500 hover:text-gray-800-500"
+            >
+              Add Employee
               <span className="px-1">
                 <BiUserPlus size={23}></BiUserPlus>
               </span>
@@ -28,9 +37,8 @@ export default function Home() {
         </div>
 
         {/* Collapsable Form */}
-        <div className="container mx-auto py-5">
-          <Form></Form>
-        </div>
+          {visible ? <Form></Form> : <></>}
+    
         {/* Table */}
         <div className="container mx-auto">
           <Table></Table>
